638a8a89ba9e6d602efbc8c839ad83018eaa4f41
vladislav blizniuk <vladislav.blizniuk@gmail.com>
2023-02-06 00:33:30 +0300

    Add homework_04 file

--- homework_04/task_04.py
+++ homework_04/task_04.py
@@ -2,10 +2,11 @@ import random
 n = random.randint(1, 10)
 while True:
     a = int(input('Enter a namber: '))
-    if a == n:
+    if int(a) == n:
         print('Well done')
         break
-        if a > n:
-            print('не угадал, ваше число больше загаданного')
-        if a < n:
-            print('не угадал, ваше число меньше загаданного')
+    elif int(a) >= n:
+        print('не угадал, ваше число больше загаданного')
+        continue
+    elif int(a) <= n:
+        print('не угадал, ваше число меньше загаданного')
