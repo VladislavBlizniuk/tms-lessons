bcc9d34d9c5558cbf36f7a6d3410c90f551f6560
vladislav blizniuk <vladislav.blizniuk@gmail.com>
2023-02-13 00:09:04 +0300

    Add homework_07 file

---
+++ homework_07/task_02.py
@@ -0,0 +1,13 @@
+# Пользователь вводит произвольное количество маленьких латинских
+# букв через пробел. Напишите функцию remove_vowels, которая принимает
+# список из этих букв и удаляет из него все гласные буквы.
+# Выведите результат работы на экран.
+
+user = list(map(str, input().lower().split()))
+glasnie = ['a', 'e', 'i', 'o', 'u']
+
+def remove_vowels(user):
+    my_list = list(filter(lambda i: i not in glasnie, user))
+    return my_list
+
+print(remove_vowels(user))
